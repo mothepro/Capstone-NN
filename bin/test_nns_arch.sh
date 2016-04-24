14d74ed69d8211539f177da120732a28d97f4e3a
Maurice Prosper <maurice.prosper@ttu.edu>
2016-04-24 03:11:04 -0500

    Continue nn where we left off

--- bin/test_nns_arch.sh
+++ bin/test_nns_arch.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")" # working directory
 cd ../
 
-mkdir -p build logs/ffnn chk/ffnn/
+mkdir -p build/ logs/ffnn chk/ffnn logs/ffdnn chk/ffdnn
 # trash logs/* build/*
 
 # Build the pickles
@@ -15,28 +15,35 @@ for i in `seq 100 100 2500`;
 do
     for h in `seq $((i)) $((i/10)) $((2*i))`;
     do
-        echo -e "Running Neural Network with ${i} input neurons & ${h} hidden neurons"
-        python src/nn/ff_nn.py \
-            -f ./build/matrix.pickle \
-            -i ${i} \
-            -n ${h} \
-            -b 100 \
-            -l 0.005 \
-            -z 75 \
-            -x 0.01 \
-            -s ./chk/ffnn/${i}-${h} \
-        > ./logs/ffnn/${i}-${h}.log
+        last_nn=$(tail -n ./logs/ffnn/${i}-${h}.log)
+        last_dnn=$(tail -n ./logs/ffdnn/${i}-${h}.log)
 
-        echo -e "Running Deep Neural Network with ${i} input neurons & ${h} hidden neurons"
-        python src/nn/ff_dnn.py \
-            -f ./build/matrix.pickle \
-            -i ${i} \
-            -n ${h} \
-            -b 100 \
-            -l 0.005 \
-            -z 75 \
-            -x 0.01 \
-            -s ./chk/ffdnn/${i}-${h} \
-        > ./logs/ffdnn/${i}-${h}.log
+        if [[ $last_nn == Finished* ]]; then
+            echo -e "Running Neural Network with ${i} input neurons & ${h} hidden neurons"
+            python src/nn/ff_nn.py \
+                -f ./build/matrix.pickle \
+                -i ${i} \
+                -n ${h} \
+                -b 100 \
+                -l 0.005 \
+                -z 75 \
+                -x 0.01 \
+                -s ./chk/ffnn/${i}-${h} \
+            > ./logs/ffnn/${i}-${h}.log
+        fi
+
+        if [[ $last_dnn == Finished* ]]; then
+            echo -e "Running Deep Neural Network with ${i} input neurons & ${h} hidden neurons"
+            python src/nn/ff_dnn.py \
+                -f ./build/matrix.pickle \
+                -i ${i} \
+                -n ${h} \
+                -b 100 \
+                -l 0.005 \
+                -z 75 \
+                -x 0.01 \
+                -s ./chk/ffdnn/${i}-${h} \
+            > ./logs/ffdnn/${i}-${h}.log
+        fi
     done
 done
