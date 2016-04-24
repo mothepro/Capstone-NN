142a0f29b0dd5cfe28f64a90df31ae6d2bb6feff
Maurice Prosper <maurice.prosper@ttu.edu>
2016-04-24 04:48:39 -0500

    Test for completion works Pickled name based on training vs test percent

--- bin/test_nns_arch.sh
+++ bin/test_nns_arch.sh
@@ -6,24 +6,28 @@ mkdir -p build/ logs/ffnn logs/ffdnn
 # trash logs/* build/*
 
 # Build the pickles
-if [ ! -f ./build/matrix.pickle ]; then
+if [ ! -f ./build/matrix-80.pickle ]; then
     echo "Building Features List"
-    python ./src/pickler.py -i ./build/matrixEnron6.txt -o ./build/matrix.pickle -t 0.8
+    python ./src/pickler.py -i ./build/matrixEnron6.txt -o ./build/matrix-80.pickle -t 0.8
 fi
 
-for i in `seq 100 100 2500`;
+for i in `seq 1000 100 2500`;
 do
     for h in `seq $((i)) $((i/10)) $((2*i))`;
     do
-        last_nn=$(tail -n 1 ./logs/ffnn/${i}-${h}.log)
-        last_dnn=$(tail -n 1 ./logs/ffdnn/${i}-${h}.log)
+        if [ -f logs/ffnn/${i}-${h}.log ]; then
+            last_nn=$(tail -n 1 logs/ffnn/${i}-${h}.log)
+        fi
+        if [ -f logs/ffdnn/${i}-${h}.log ]; then
+            last_dnn=$(tail -n 1 logs/ffdnn/${i}-${h}.log)
+        fi
 
-        if [[ $last_nn == Finished* ]]; then
+        if [[ $last_nn != Finished* ]]; then
             echo -e "Running Neural Network with ${i} input neurons & ${h} hidden neurons"
 
             mkdir -p chk/ffnn/${i}/${h}
             python src/nn/ff_nn.py --train \
-                -f ./build/matrix.pickle \
+                -f ./build/matrix-80.pickle \
                 -i ${i} \
                 -n ${h} \
                 -b 100 \
@@ -31,15 +35,15 @@ do
                 -z 75 \
                 -x 0.005 \
                 -s ./chk/ffnn/${i}/${h}/weights \
-            > ./logs/ffnn/${i}-${h}.log
+            > logs/ffnn/${i}-${h}.log
         fi
 
-        if [[ $last_dnn == Finished* ]]; then
+        if [[ $last_dnn != Finished* ]]; then
             echo -e "Running Deep Neural Network with ${i} input neurons & ${h} hidden neurons"
 
             mkdir -p chk/ffdnn/${i}/${h}
             python src/nn/ff_dnn.py --train \
-                -f ./build/matrix.pickle \
+                -f ./build/matrix-80.pickle \
                 -i ${i} \
                 -n ${h} \
                 -b 100 \
@@ -47,7 +51,7 @@ do
                 -z 75 \
                 -x 0.005 \
                 -s ./chk/ffdnn/${i}/${h}/weights \
-            > ./logs/ffdnn/${i}-${h}.log
+            > logs/ffdnn/${i}-${h}.log
         fi
     done
 done
